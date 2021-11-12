7bae2d9b1855684bc24fe88f47e9d3456bcaa61e
bruna-gregorio <blgregorio16@gmail.com>
2021-11-12 13:08:13 -0300

    autenticando o usuario

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express"
 
+import { ensureAuthenticate } from "./middleware/ensureAuthenticate"
+import { ensureAdmin } from "./middleware/ensureAdmin"
+
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
+
 import { CreateCourseController } from "./controllers/CreateCourseController"
 import { CreateUserController } from "./controllers/CreateUserController"
 import { DeleteUserController } from "./controllers/DeleteUserController"
@@ -8,6 +13,8 @@ import { UpdateUserController } from "./controllers/UpdateUserController"
 
 const routes = Router()
 
+const authenticateUserController = new AuthenticateUserController()
+
 const createUserController = new CreateUserController()
 const listUserController = new ListUserController()
 const updateUserController = new UpdateUserController()
@@ -15,15 +22,17 @@ const deleteUserController = new DeleteUserController()
 
 const createCourseController = new CreateCourseController()
 
+routes.post("/login", authenticateUserController.handle)
+
 //users
 routes.post("/users", createUserController.handle)
-routes.get("/users", listUserController.handle)
-routes.put("/users/name/:id", updateUserController.updateName)
-routes.put("/users/email/:id", updateUserController.updateEmail)
-routes.put("/users/password/:id", updateUserController.updatePassword)
-routes.delete("/users/:id", deleteUserController.handle)
+routes.get("/users", ensureAuthenticate, listUserController.handle)
+routes.put("/users/name/:id", ensureAuthenticate, updateUserController.updateName)
+routes.put("/users/email/:id", ensureAuthenticate, updateUserController.updateEmail)
+routes.put("/users/password/:id", ensureAuthenticate, updateUserController.updatePassword)
+routes.delete("/users/:id", ensureAuthenticate, deleteUserController.handle)
 
 //courses
-routes.post("/courses", createCourseController.handle)
+routes.post("/courses", ensureAuthenticate, ensureAdmin, createCourseController.handle)
 
 export { routes }
